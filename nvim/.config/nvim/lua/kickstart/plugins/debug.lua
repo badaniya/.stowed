befa71d08331d09e275c2b737200507e19f96a5b
badaniya <badaniya@extremenetworks.com>
2024-10-01 21:10:04 -0700

    nvim: Reorganize debug file

--- nvim/.config/nvim/lua/kickstart/plugins/debug.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/debug.lua
@@ -31,6 +31,11 @@ return {
     local dap = require 'dap'
     local dapui = require 'dapui'
 
+    -- Dap listeners
+    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
     dapui.setup {
@@ -99,9 +104,5 @@ return {
 
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
-
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
   end,
 }
