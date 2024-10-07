c20ccd0c6b01e3d19a14659729857960d07759a0
badaniya <badaniya@extremenetworks.com>
2024-10-07 09:44:40 -0700

    nvim: Attempt to fix code coverage report to have relative paths

--- nvim/.config/nvim/lua/custom/plugins/nvim-coverage.lua
+++ nvim/.config/nvim/lua/custom/plugins/nvim-coverage.lua
@@ -12,22 +12,49 @@ return {
       },
     },
     commands = true, -- create commands
+    load_coverlge_cb = function(ftype)
+      local config = require 'coverage.config'
+      local project_dir = 'GoDCApp/NVO'
+      local file = io.open(config.opts.lang.go.coverage_file, 'r')
+      local fileContent = {}
+
+      if not (file == nil) then
+        for line in file:lines() do
+          if string.match(line, project_dir) then
+            local name = string.gsub(line, '.*' .. project_dir .. '(.*)', '%1')
+            table.insert(fileContent, name)
+          end
+        end
+        io.close(file)
+      end
+
+      file = io.open(config.opts.lang.go.coverage_file, 'w')
+
+      if not (file == nil) then
+        for _, value in ipairs(fileContent) do
+          file:write(value .. '\n')
+        end
+        io.close(file)
+      end
+
+      -- notify that the coverage file has been loaded
+      vim.notify('Loaded ' .. ftype .. ' coverage')
+    end,
     highlights = {
       -- customize highlight groups created by the plugin
       covered = { fg = '#C3E88D' }, -- supports style, fg, bg, sp (see :h highlight-gui)
       uncovered = { fg = '#F07178' },
+      partial = { fg = '#AA71F0' },
     },
     signs = {
       -- use your own highlight groups or text markers
       covered = { hl = 'CoverageCovered', text = '▎' },
       uncovered = { hl = 'CoverageUncovered', text = '▎' },
+      partial = { hl = 'CoveragePartial', text = '▎' },
     },
     summary = {
       -- customize the summary pop-up
       min_coverage = 80.0, -- minimum coverage threshold (used for highlighting)
-    },
-    lang = {
-      -- customize language specific settings
     },
   },
 
