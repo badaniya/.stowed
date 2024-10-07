94df639f45ac10de1a1e0b057ba1e50d1497c33d
badaniya <badaniya@extremenetworks.com>
2024-10-07 12:00:19 -0700

    nvim: Update coverage callback to fixup the coverage report paths

--- nvim/.config/nvim/lua/custom/plugins/nvim-coverage.lua
+++ nvim/.config/nvim/lua/custom/plugins/nvim-coverage.lua
@@ -13,31 +13,42 @@ return {
     },
     commands = true, -- create commands
     load_coverage_cb = function(ftype)
-      local config = require 'coverage.config'
-      local project_dir = 'GoDCApp/NVO'
-      local fileContent = {}
+      -- The `go` coverage.out file contain the prefixed github repo path
+      -- but needs the relative replacement path to reference the local code.
+      -- The coverage file paths neeed to be converted from github paths to
+      -- local replacement paths.
+      if ftype == 'go' then
+        local config = require 'coverage.config'
+        local project_dir = 'GoDCApp/NVO'
+        local coverage_mode = 'mode:'
+        local fileContent = {}
 
-      local file = io.open(config.opts.lang.go.coverage_file, 'r')
-      vim.notify('Loading coverage file ' .. config.opts.lang.go.coverage_file .. ' ...')
-
-      if not (file == nil) then
-        for line in file:lines() do
-          if string.match(line, project_dir) then
-            local name = string.gsub(line, '.*' .. project_dir .. '(.*)', '%1')
-            table.insert(fileContent, name)
-          end
-        end
-        io.close(file)
-      end
-
-      if not (next(fileContent) == nil) then
-        file = io.open(config.opts.lang.go.coverage_file, 'w')
+        local file = io.open(config.opts.lang.go.coverage_file, 'r')
+        vim.notify('Loading coverage file ' .. config.opts.lang.go.coverage_file .. ' ...')
 
         if not (file == nil) then
-          for _, value in ipairs(fileContent) do
-            file:write(value .. '\n')
+          for line in file:lines() do
+            if string.match(line, project_dir) then
+              -- Match all GoDCApp/NVO paths
+              local name = string.gsub(line, '.*' .. project_dir .. '(.*)', '%1')
+              table.insert(fileContent, name)
+            elseif string.match(line, '^' .. coverage_mode) then
+              -- Match mode: <atomic|set|count ...> in the coverage.out file
+              table.insert(fileContent, line)
+            end
           end
           io.close(file)
+        end
+
+        if not (next(fileContent) == nil) then
+          file = io.open(config.opts.lang.go.coverage_file, 'w')
+
+          if not (file == nil) then
+            for _, value in ipairs(fileContent) do
+              file:write(value .. '\n')
+            end
+            io.close(file)
+          end
         end
       end
 
