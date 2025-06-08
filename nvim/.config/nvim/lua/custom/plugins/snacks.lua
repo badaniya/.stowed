6f0f3312f3c4a96d6ee2393e08b1314193f92ac9
badaniya <badaniya@extremenetworks.com>
2025-06-07 23:21:43 -0700

    nvim: snacks a github centric dashboard

--- nvim/.config/nvim/lua/custom/plugins/snacks.lua
+++ nvim/.config/nvim/lua/custom/plugins/snacks.lua
@@ -8,7 +8,87 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
+    dashboard = {
+      enabled = true,
+      ---@class snacks.dashboard.Config
+      ---@field enabled? boolean
+      ---@field sections snacks.dashboard.Section
+      ---@field formats table<string, snacks.dashboard.Text|fun(item:snacks.dashboard.Item, ctx:snacks.dashboard.Format.ctx):snacks.dashboard.Text>
+      sections = {
+        { section = 'header' },
+        {
+          pane = 2,
+          section = 'terminal',
+          cmd = 'colorscript -e square',
+          height = 5,
+          padding = 1,
+        },
+        { section = 'keys', gap = 1, padding = 1 },
+        {
+          pane = 2,
+          icon = ' ',
+          desc = 'Browse Repo',
+          padding = 1,
+          key = 'b',
+          action = function()
+            Snacks.gitbrowse()
+          end,
+        },
+        function()
+          local in_git = Snacks.git.get_root() ~= nil
+          local cmds = {
+            {
+              title = 'Notifications',
+              cmd = 'gh notify -s -a -n5',
+              action = function()
+                vim.ui.open 'https://github.com/notifications'
+              end,
+              key = 'n',
+              icon = ' ',
+              height = 5,
+              enabled = true,
+            },
+            {
+              title = 'Open Issues',
+              cmd = 'gh issue list -L 3',
+              key = 'i',
+              action = function()
+                vim.fn.jobstart('gh issue list --web', { detach = true })
+              end,
+              icon = ' ',
+              height = 7,
+            },
+            {
+              icon = ' ',
+              title = 'Open PRs',
+              cmd = 'gh pr list -L 3',
+              key = 'P',
+              action = function()
+                vim.fn.jobstart('gh pr list --web', { detach = true })
+              end,
+              height = 7,
+            },
+            {
+              icon = ' ',
+              title = 'Git Status',
+              cmd = 'git --no-pager diff --stat -B -M -C',
+              height = 10,
+            },
+          }
+          return vim.tbl_map(function(cmd)
+            return vim.tbl_extend('force', {
+              pane = 2,
+              section = 'terminal',
+              enabled = in_git,
+              padding = 1,
+              ttl = 5 * 60,
+              indent = 3,
+            }, cmd)
+          end, cmds)
+        end,
+        { section = 'startup' },
+      },
+    },
     explorer = { enabled = true },
     indent = { enabled = true },
     image = {
