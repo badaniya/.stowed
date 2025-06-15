ffa7fca556ba64cd4825ec44ac087233b53c302a
badaniya <badaniya@extremenetworks.com>
2025-06-15 16:44:54 -0700

    nvim: Keybindings for folding are already setup

--- nvim/.config/nvim/lua/custom/plugins/nvim-ufo.lua
+++ nvim/.config/nvim/lua/custom/plugins/nvim-ufo.lua
@@ -33,8 +33,9 @@ return {
       vim.o.foldenable = true
       vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
-      vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-      vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+      -- These are already set
+      --vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+      --vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
     end,
 
     config = function(_, opts)
