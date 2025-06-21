c8a8d7d0b433c99c954bd5b51a570828380e5f5d
badaniya <badaniya@extremenetworks.com>
2025-06-20 19:49:41 -0700

    nvim: nvim-origami - update fold status spacing with snacks statuscolumn

--- nvim/.config/nvim/lua/custom/plugins/nvim-origami.lua
+++ nvim/.config/nvim/lua/custom/plugins/nvim-origami.lua
@@ -20,7 +20,7 @@ return {
       foldtext = {
         enabled = true,
         lineCount = {
-          template = '  …  󰘖  %d', -- `%d` is repalced with the number of folded lines
+          template = '…  󰘖  %d', -- `%d` is repalced with the number of folded lines
           hlgroup = 'Comment',
         },
         diagnostics = {
