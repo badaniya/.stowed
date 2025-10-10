fdf9f0961e1d398868946cd62f7d7a4bd11878c1
badaniya <badaniya@extremenetworks.com>
2025-10-09 18:32:12 -0700

    nvim: Add sqruff for SQL linting/formatting.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -606,6 +606,7 @@ require('lazy').setup({
         'revive',
         'shellcheck',
         'spectral-language-server',
+        'sqruff',
         'sqls',
         'staticcheck',
         'stylua',
@@ -694,7 +695,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, sql = true }
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -708,6 +709,8 @@ require('lazy').setup({
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
+        --sql = { 'sqruff' },
+        --pgsql = { 'sqruff' },
       },
     },
   },
