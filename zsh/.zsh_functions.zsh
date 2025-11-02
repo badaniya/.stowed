088898cedce4adcba35b7dcaa83ec390ad90152a
badaniya <badaniya@extremenetworks.com>
2025-11-02 12:14:53 -0800

    zsh: Set the GIT_DEV_REPO_PATH ENV when starting nvim

--- zsh/.zsh_functions.zsh
+++ zsh/.zsh_functions.zsh
@@ -1,21 +1,6 @@
 #GLOBALS
-LINUX_HOST1="10.24.12.84"
-LINUX_HOST2="10.24.12.86"
-CC_HOST="badaniya-vm"
-BUILD_HOST="$CC_HOST"
-FWDL_HOST="$LINUX_HOST2"
-SS_PATH="$HOME/ss_team"
-DOMAIN_NAME="extremenetworks.com"
-BASE_SRC="$HOME/workspace/slxos"
 MASTER_REPO_SRC="$HOME/workspace/badaniya/master"
-
-CLEARTOOL=/usr/atria/bin/cleartool
-
 NUM_CPU_CORES=`cat /proc/cpuinfo | grep processor | wc -l`
-
-SWITCH_TERMINAL_GEOMETRY="89x25"
-CHASSIS_TERMINAL_GEOMETRY="180x25"
-FULL_TERMINAL_GEOMETRY="180x56"
 
 if [[ -f $HOME/.private_zsh_functions.zsh ]]; then
     source $HOME/.private_zsh_functions.zsh
@@ -29,12 +14,16 @@ preexec()
         # This sets up the goose config file with env variables before running any goose command
         envsubst < $HOME/.config/goose/config.yaml.in > $HOME/.config/goose/config.yaml
         ;;
+    nvim*)
+        # This sets up the git repo directory ENV when nvim is launched
+        export GIT_DEV_REPO_PATH=$(git rev-parse --show-toplevel 2> /dev/null)
+        ;;
     esac
 }
 
 host_ip()
 {
-    hostname -I | cut -d ' ' -f 1
+    ip route get 1.1.1.1 | awk '{print $7}' | head -1
 }
 
 # FUNCTIONS
@@ -147,236 +136,13 @@ function list_worktree()
     popd > /dev/null 2>&1
 }
 
-swagger_codegen()
+function git_ls_lrt ()
 {
-    java -jar /usr/bin/swagger-codegen-cli.jar "$@"
+    for k in `git branch | sed s/^..//`; do
+        echo -e `git log -1 --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" "$k" 2>/dev/null`\\t"$k";
+    done | sort -r | less -r
 }
-
-get_postgresql_url() 
-{
-    local USAGE="get_postgresql_url <postgres user> <postgres pass> <postgres db name>"
-
-    if [[ $# -lt 3 || "$1" =~ (^-h$|^--help$) ]]; then
-        echo "$USAGE"
-        return 1
-    fi
-
-    local POSTGRES_USER="$1"
-    local POSTGRES_PASS="$2"
-    local POSTGRES_DBNAME="$3"
-    local POSTGRES_IPPORT=`get_k3sdb_ipport`
-
-    echo "postgresql://$POSTGRES_USER:$POSTGRES_PASS@$POSTGRES_IPPORT/$POSTGRES_DBNAME?sslmode=disable"
-}
-
-enable_nonroot_k3s() {
-    sudo chmod 777 /etc/rancher/k3s/k3s.yaml
-}
-
-ps_docker()
-{
-    local USAGE="ps_docker [container|image|names]"
-    INPUT="$1"
-
-    if [[ "$INPUT" == "help" ]]; then
-        echo "$USAGE"
-        return 1
-    fi
-    
-    if [[ -z "$INPUT" ]]; then
-        INPUT="container"
-    fi
-
-    if [[ "$INPUT" == "container" ]]; then
-        docker ps -a --format "{{.ID}}"
-    elif [[ "$INPUT" == "image" ]]; then
-        docker ps -a --format "table {{.ID}}\t{{.Image}}"
-    elif [[ "$INPUT" == "names" ]]; then
-        docker ps -a --format "table {{.ID}}\t{{.Names}}\t{{.Image}}"
-    fi 
-}
-
-rm_docker()
-{
-    local USAGE="rm_docker <all | container id>"
-    INPUT="$1"
-
-    if [[ $# -lt 1 || $# -ge 2 || "$1" =~ (^-h$|^--help$) ]]; then
-        echo "$USAGE"
-        return 1
-    fi
-
-    if [[ "$INPUT" == "all" ]]; then 
-        for container_id in `ps_docker`; do
-            docker rm -vf $container_id
-        done
-    else
-        docker rm -vf "$INPUT"
-    fi
-}
-
-exec_docker()
-{
-    local USAGE="exec_docker <container id or name> [shell]"
-
-    if [[ $# -lt 1 || $# -ge 3 || "$1" =~ (^-h$|^--help$) ]]; then
-        echo "$USAGE"
-        return 1
-    fi
-
-    local CONTAINER="$1"   
-    local INSHELL="$2"
-
-    if [[ -z "$INSHELL" ]]; then
-        INSHELL="ash"
-    fi 
-
-    echo docker exec -it "$CONTAINER" "$INSHELL"
-    docker exec -it "$CONTAINER" "$INSHELL"
-}
-
-ps_kube()
-{
-    get_kube pods "$@"
-}
-
-get_kube()
-{
-    local USAGE="get_kube [options]"
-
-    local OPTIONS="$1"
-
-    if [[ "$OPTIONS" == "help" ]]; then
-        echo "$USAGE"
-        return 1
-    fi
-
-    #echo k3s kubectl -n efa get "$@"
-    k3s kubectl -n efa get "$@"
-}
-
-rm_kube()
-{
-    local USAGE="rm_kube <service name>"
-
-    if [[ $# -lt 1 || "$1" =~ (^-h$|^--help$) ]]; then
-        echo "$USAGE"
-        return 1
-    fi
-
-    local SERVICE_NAME="$1"
-    local KUBE_SERVICES=(`k3s kubectl -n efa get all | tac | awk '{print $1}' | grep $SERVICE_NAME`)
-
-    echo "KUBE_SERVICES:" 
-
-    for service in "${KUBE_SERVICES[@]}"; do
-        echo "- $service"
-        k3s kubectl -n efa delete $service
-    done
-}
-
-delete_kube()
-{
-    local USAGE="delete_kube [options]"
-
-    local OPTIONS="$1"
-
-    if [[ "$OPTIONS" == "help" ]]; then
-        echo "$USAGE"
-        return 1
-    fi
-
-    #echo k3s kubectl -n efa delete "$@"
-    k3s kubectl -n efa delete "$@"
-}
-
-create_kube()
-{
-    local USAGE="create_kube [options]"
-
-    local OPTIONS="$1"
-
-    if [[ "$OPTIONS" == "help" ]]; then
-        echo "$USAGE"
-        return 1
-    fi
-
-    k3s kubectl -n efa create "$@"
-}
-
-apply_kube()
-{
-    local USAGE="apply_kube [options]"
-
-    local OPTIONS="$1"
-
-    if [[ "$OPTIONS" == "help" ]]; then
-        echo "$USAGE"
-        return 1
-    fi
-
-    k3s kubectl -n efa apply "$@"
-}
-
-describe_kube()
-{
-    local USAGE="describe_kube [options]"
-
-    local OPTIONS="$1"
-
-    if [[ "$OPTIONS" == "help" ]]; then
-        echo "$USAGE"
-        return 1
-    fi
-
-    k3s kubectl -n efa describe "$@"
-}
-
-exec_kube()
-{
-    local USAGE="exec_kube <pod name> [shell]"
-
-    if [[ $# -lt 1 || $# -ge 3 || "$1" =~ (^-h$|^--help$) ]]; then
-        echo "$USAGE"
-        return 1
-    fi
-
-    local PODNAME="$1"
-    local INSHELL="$2"
-
-    if [[ -z "$INSHELL" ]]; then
-        INSHELL="ash"
-    fi
-
-    local CONTAINER=`ps_kube | grep $PODNAME | cut -d ' ' -f 1`
-
-    echo k3s kubectl -n efa exec "$CONTAINER" -it -- "$INSHELL"
-    k3s kubectl -n efa exec "$CONTAINER" -it -- "$INSHELL"
-}
-
-log_kube()
-{
-    local USAGE="log_kube <pod name> [options]"
-
-    if [[ $# -lt 1 || "$1" =~ (^-h$|^--help$) ]]; then
-        echo "$USAGE"
-        return 1
-    fi
-
-    local CONTAINER="$1"
-    shift
-    local OPTIONS="$@"
-
-    echo k3s kubectl -n efa logs "$CONTAINER" "$@"
-    k3s kubectl -n efa logs "$CONTAINER" "$@"
-}
-
-set_term_title()
-{
-    echo -en "\033]0;$1\a"
-}
-
-update_gopls()
+function update_gopls()
 {
     local USAGE="update_gopls [version]"
 
@@ -399,122 +165,74 @@ update_gopls()
     GOPATH="$ORIGINAL_GOPATH"
 }
 
-function git-ls-lrt ()
+function swagger_codegen()
 {
-    for k in `git branch | sed s/^..//`; do
-        echo -e `git log -1 --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" "$k" 2>/dev/null`\\t"$k";
-    done | sort -r | less -r
+    java -jar /usr/bin/swagger-codegen-cli.jar "$@"
 }
 
-function is_xauth_locked()
+function get_postgresql_url() 
 {
-    local IS_XAUTH_LOCKED="yes"
-
-    IS_XAUTH_LOCKED=`xauth info | grep "File locked" | awk '{print $3}'`
-
-    echo $IS_XAUTH_LOCKED
-}
-
-function wait_for_xauth_lock()
-{
-    local IS_XAUTH_LOCKED=`is_xauth_locked`
-
-    while [ "$IS_XAUTH_LOCKED" != "no" ]; do
-        IS_XAUTH_LOCKED=`is_xauth_locked`
-        sleep 0.2
-    done
-}
-
-# Function: terminal_launcher
-# Description: This will launch a preset multi-tabbed gnome terminal.  When chassis or switch is specified with a label then all terminal session output
-#              Is logged to an appropriate labeled file under $HOME/sessionlogs.
-function terminal_launcher()
-{
-    local USAGE="Usage: terminal_launcher < <dev|code|sim> <server host name to SSH into> | <chassis|switch> <Label for the device> >"
+    local USAGE="get_postgresql_url <postgres user> <postgres pass> <postgres db name>"
 
     if [[ $# -lt 3 || "$1" =~ (^-h$|^--help$) ]]; then
+        echo "$USAGE"
+        return 1
+    fi
+
+    local POSTGRES_USER="$1"
+    local POSTGRES_PASS="$2"
+    local POSTGRES_DBNAME="$3"
+    local POSTGRES_IPPORT=`get_k3sdb_ipport`
+
+    echo "postgresql://$POSTGRES_USER:$POSTGRES_PASS@$POSTGRES_IPPORT/$POSTGRES_DBNAME?sslmode=disable"
+}
+
+function get_private_shared_memory()
+{
+    local USAGE="Usage: get_private_shared_memory <IP Address>"
+    local HOST_IP="$1"
+    local HOST_CMD='smaps -t `pidof Dcmd.Linux.powerpc` | grep Total | sed -n "s/.* \([0-9]*\) KB | Total$/\1/p"'
+    local HOST_OPTIONS='-o PubKeyAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+
+    if [[ $# -lt 1 || "$1" =~ (^-h$|^--help$) ]]; then
         echo $USAGE
         return 1
     fi
 
+    expect_ssh "$HOST_IP" "root" "fibranne" "$HOST_OPTIONS" "$HOST_CMD"
+}
 
-    local TERMINAL="mate-terminal"
-    local TERM_CLASS=$2
-    local PARAM=$3
+function get_process_file_descriptor_usage()
+{
+    local USAGE="Usage: get_process_file_descriptor_usage <Process Name> <IP Address>"
+    local PROCESS_NAME="$1"
+    local HOST_IP="$2"
+    local HOST_CMD='ls -1 /proc/`pidof '$PROCESS_NAME'`/fd | wc -l'
+    local HOST_OPTIONS='-o PubKeyAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 
-    # Create a session log directory
-    if [[ "$TERM_CLASS" == "switch" || "$TERM_CLASS" == "chassis" && -n "$PARAM" ]]; then
-        if [ ! -d $HOME/sessionlogs ]; then
-            mkdir $HOME/sessionlogs
-        fi
+    if [[ $# -lt 2 || "$1" =~ (^-h$|^--help$) ]]; then
+        echo $USAGE
+        return 1
     fi
 
+    expect_ssh "$HOST_IP" "root" "fibranne" "$HOST_OPTIONS" "$HOST_CMD"
+}
 
-    if [ "$TERM_CLASS" == "code" ]; then
-        if [ -z "$PARAM" ]; then
-            PARAM=$BUILD_HOST
-        fi
-
-        $TERMINAL --geometry=$FULL_TERMINAL_GEOMETRY \
-            --tab --title=editor1 --command "bash -c 'while [ \"no\" == `xauth info | sed -n 's/File locked:[ ]*\([a-z]*\)/\1/p'` ]; do ssh -Yt $PARAM; sleep 0.0; done"\' --active \
-            --tab --title=editor2 --command "bash -c 'while [ \"no\" == `xauth info | sed -n 's/File locked:[ ]*\([a-z]*\)/\1/p'` ]; do ssh -Yt $PARAM; sleep 0.1; done"\' \
-            --tab --title=editor3 --command "bash -c 'while [ \"no\" == `xauth info | sed -n 's/File locked:[ ]*\([a-z]*\)/\1/p'` ]; do ssh -Yt $PARAM; sleep 0.2; done"\' \
-            --tab --title=build   --command "bash -c 'while [ \"no\" == `xauth info | sed -n 's/File locked:[ ]*\([a-z]*\)/\1/p'` ]; do ssh -Yt $PARAM; sleep 0.3; done"\'
-    elif [ "$TERM_CLASS" == "sim" ]; then 
-        if [ -z "$PARAM" ]; then
-            PARAM=$BUILD_HOST
-        fi
-
-        $TERMINAL --geometry=$FULL_TERMINAL_GEOMETRY \
-            --tab --title=dcm1      --command "bash -c 'while [ \"no\" == `xauth info | sed -n 's/File locked:[ ]*\([a-z]*\)/\1/p'` ]; do ssh -Yt $PARAM; sleep 0.0; done"\' --active \
-            --tab --title=dcm2      --command "bash -c 'while [ \"no\" == `xauth info | sed -n 's/File locked:[ ]*\([a-z]*\)/\1/p'` ]; do ssh -Yt $PARAM; sleep 0.1; done"\' \
-            --tab --title=dcm3      --command "bash -c 'while [ \"no\" == `xauth info | sed -n 's/File locked:[ ]*\([a-z]*\)/\1/p'` ]; do ssh -Yt $PARAM; sleep 0.2; done"\' \
-            --tab --title=dcmclient --command "bash -c 'while [ \"no\" == `xauth info | sed -n 's/File locked:[ ]*\([a-z]*\)/\1/p'` ]; do ssh -Yt $PARAM; sleep 0.3; done"\' \
-            --tab --title=confdcli  --command "bash -c 'while [ \"no\" == `xauth info | sed -n 's/File locked:[ ]*\([a-z]*\)/\1/p'` ]; do ssh -Yt $PARAM; sleep 0.4; done"\' \
-            --tab --title=misc_gdb  --command "bash -c 'while [ \"no\" == `xauth info | sed -n 's/File locked:[ ]*\([a-z]*\)/\1/p'` ]; do ssh -Yt $PARAM; sleep 0.6; done"\' 
-    elif [ "$TERM_CLASS" == "dev" ]; then
-        terminal_launcher $TERMINAL code $PARAM&
-        sleep 2
-        terminal_launcher $TERMINAL sim $PARAM
-
-    elif [ "$TERM_CLASS" == "switch" ]; then
-        if [ -z "$PARAM" ]; then
-            $TERMINAL --geometry=$SWITCH_TERMINAL_GEOMETRY \
-                --tab --active -t Console \
-                --tab -t Telnet \
-                --tab -t Telnet \
-                --tab -t Telnet
-        else
-            $TERMINAL --geometry=$SWITCH_TERMINAL_GEOMETRY \
-                --tab --active -t "$PARAM Console" -e "script -afq $HOME/sessionlogs/$PARAM.Console.`date +%Y%m%d.%H%M%S`.txt" \
-                --tab -t "$PARAM Telnet1" -e "script -afq $HOME/sessionlogs/$PARAM.Telnet1.`date +%Y%m%d.%H%M%S`.txt" \
-                --tab -t "$PARAM Telnet2" -e "script -afq $HOME/sessionlogs/$PARAM.Telnet2.`date +%Y%m%d.%H%M%S`.txt" \
-                --tab -t "$PARAM Telnet3" -e "script -afq $HOME/sessionlogs/$PARAM.Telnet3.`date +%Y%m%d.%H%M%S`.txt"
-        fi
-    elif [ "$TERM_CLASS" == "chassis" ]; then
-        if [ -z "$PARAM" ]; then
-            $TERMINAL --geometry=$CHASSIS_TERMINAL_GEOMETRY \
-                --tab --active -t "MM1 Console" \
-                --tab -t "MM2 Console" \
-                --tab -t "LC# Console" \
-                --tab -t "LC# Console" \
-                --tab -t Telnet \
-                --tab -t Telnet \
-                --tab -t Telnet
-        else
-            $TERMINAL --geometry=$CHASSIS_TERMINAL_GEOMETRY \
-                --tab --active -t "$PARAM MM1 Console" -e "script -afq $HOME/sessionlogs/$PARAM.MM1.Console.`date +%Y%m%d.%H%M%S`.txt" \
-                --tab -t "$PARAM MM2 Console" -e "script -afq $HOME/sessionlogs/$PARAM.MM2.Console.`date +%Y%m%d.%H%M%S`.txt" \
-                --tab -t "$PARAM LC# Console" -e "script -afq $HOME/sessionlogs/$PARAM.LC1.Console.`date +%Y%m%d.%H%M%S`.txt" \
-                --tab -t "$PARAM LC# Console" -e "script -afq $HOME/sessionlogs/$PARAM.LC2.Console.`date +%Y%m%d.%H%M%S`.txt" \
-                --tab -t "$PARAM Telnet1" -e "script -afq $HOME/sessionlogs/$PARAM.Telnet1.`date +%Y%m%d.%H%M%S`.txt" \
-                --tab -t "$PARAM Telnet2" -e "script -afq $HOME/sessionlogs/$PARAM.Telnet2.`date +%Y%m%d.%H%M%S`.txt" \
-                --tab -t "$PARAM Telnet3" -e "script -afq $HOME/sessionlogs/$PARAM.Telnet3.`date +%Y%m%d.%H%M%S`.txt"
-        fi
-    elif [ "$TERM_CLASS" == "full" ]; then
-        $TERMINAL --geometry=$FULL_TERMINAL_GEOMETRY
+function free_swap_mem()
+{
+    local FREE_MEM="$(free | grep 'Mem:' | awk '{print $7}')"
+    local USED_SWAP="$(free | grep 'Swap:' | awk '{print $3}')"
+    
+    echo -e "Free memory:\t$FREE_MEM kB ($((FREE_MEM / 1024)) MiB)\nUsed swap:\t$USED_SWAP kB ($((USED_SWAP / 1024)) MiB)"
+    if [[ $USED_SWAP -eq 0 ]]; then
+        echo "Congratulations! No swap is in use."
+    elif [[ $USED_SWAP -lt $FREE_MEM ]]; then
+        echo "Freeing swap..."
+        sudo swapoff -a
+        sudo swapon -a
     else
-        $TERMINAL
+        echo "Not enough free memory. Exiting."
+        exit 1
     fi
 }
 
@@ -810,53 +528,4 @@ function multiple_expect_telnet()
     set -m
 
     echo "[multiple_expect_telnet completed.]"
-}
-
-function get_private_shared_memory()
-{
-    local USAGE="Usage: get_private_shared_memory <IP Address>"
-    local HOST_IP="$1"
-    local HOST_CMD='smaps -t `pidof Dcmd.Linux.powerpc` | grep Total | sed -n "s/.* \([0-9]*\) KB | Total$/\1/p"'
-    local HOST_OPTIONS='-o PubKeyAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
-
-    if [[ $# -lt 1 || "$1" =~ (^-h$|^--help$) ]]; then
-        echo $USAGE
-        return 1
-    fi
-
-    expect_ssh "$HOST_IP" "root" "fibranne" "$HOST_OPTIONS" "$HOST_CMD"
-}
-
-function get_process_file_descriptor_usage()
-{
-    local USAGE="Usage: get_process_file_descriptor_usage <Process Name> <IP Address>"
-    local PROCESS_NAME="$1"
-    local HOST_IP="$2"
-    local HOST_CMD='ls -1 /proc/`pidof '$PROCESS_NAME'`/fd | wc -l'
-    local HOST_OPTIONS='-o PubKeyAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
-
-    if [[ $# -lt 2 || "$1" =~ (^-h$|^--help$) ]]; then
-        echo $USAGE
-        return 1
-    fi
-
-    expect_ssh "$HOST_IP" "root" "fibranne" "$HOST_OPTIONS" "$HOST_CMD"
-}
-
-function free_swap_mem()
-{
-    local FREE_MEM="$(free | grep 'Mem:' | awk '{print $7}')"
-    local USED_SWAP="$(free | grep 'Swap:' | awk '{print $3}')"
-    
-    echo -e "Free memory:\t$FREE_MEM kB ($((FREE_MEM / 1024)) MiB)\nUsed swap:\t$USED_SWAP kB ($((USED_SWAP / 1024)) MiB)"
-    if [[ $USED_SWAP -eq 0 ]]; then
-        echo "Congratulations! No swap is in use."
-    elif [[ $USED_SWAP -lt $FREE_MEM ]]; then
-        echo "Freeing swap..."
-        sudo swapoff -a
-        sudo swapon -a
-    else
-        echo "Not enough free memory. Exiting."
-        exit 1
-    fi
 }
